cb682b11d1ca7b8a42f3ab31279dd6a0a60326fd
Stepa075 <stepa0751@gmail.com>
2023-03-27 19:03:56 +0300

    Initial commit

---
+++ app/src/main/java/com/stepa075/roulettecompose/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.stepa075.roulettecompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val GreenBg = Color(0xFF26A341)
+val Red = Color(0xFFD30D3B)
