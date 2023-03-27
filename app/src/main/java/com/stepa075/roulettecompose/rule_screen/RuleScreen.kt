aafcc34b9a584801912b7d32a6d970638b2309b0
Stepa075 <stepa0751@gmail.com>
2023-03-27 23:17:16 +0300

    App is complete

--- app/src/main/java/com/stepa075/roulettecompose/rule_screen/RuleScreen.kt
+++ app/src/main/java/com/stepa075/roulettecompose/rule_screen/RuleScreen.kt
@@ -1,12 +1,16 @@
 package com.stepa075.roulettecompose.rule_screen
 
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -14,15 +18,39 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.stepa075.roulettecompose.R
 import com.stepa075.roulettecompose.ui.theme.Reed
+import com.stepa075.roulettecompose.utils.NumberUtil
+import kotlin.math.roundToInt
 
 @Composable
 fun RuleScreen() {
+
+
+
+    var rotationValue by remember{
+        mutableStateOf(0f)
+    }
+    var number by remember {
+        mutableStateOf(0)
+    }
+    val angle: Float by animateFloatAsState(targetValue = rotationValue,
+        animationSpec = tween(durationMillis = 2000,
+            easing = LinearOutSlowInEasing),
+        finishedListener = {
+            val indexOfArray = (360f - (it % 360)) / (360f / NumberUtil.list.size)
+            number = NumberUtil.list[indexOfArray.roundToInt()]
+        }
+        )
+
     Column(modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
-                modifier = Modifier.fillMaxWidth().height(100.dp).wrapContentHeight().wrapContentWidth(),
-                text = "0",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .wrapContentHeight()
+                    .wrapContentWidth(),
+                text = number.toString(),
             fontWeight = FontWeight.Bold,
                 fontSize = 35.sp,
                 color = Color.White
@@ -33,7 +61,9 @@ fun RuleScreen() {
                 Image(
                     painter = painterResource(id = R.drawable.ruleta),
                     contentDescription = "ruleta",
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .rotate(angle)
                 )
 
                 Image(
@@ -44,9 +74,13 @@ fun RuleScreen() {
                 
 
             }
-        Button(onClick = { /*TODO*/ },
+        Button(onClick = {
+                         rotationValue = (720..1080).random().toFloat() + angle
+        },
             colors = ButtonDefaults.buttonColors(backgroundColor = Reed),
-        modifier = Modifier.fillMaxWidth().padding(10.dp)) {
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)) {
             Text(text = "Start",
                 color = Color.White)
         }
